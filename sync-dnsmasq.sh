ce75efaeea096553c607a5bdfc39e65c3e701068
goose-ws <goose@captain-kickass.net>
2021-02-20 15:02:39 -0500

    Update sync-dnsmasq.sh

--- sync-dnsmasq.sh
+++ sync-dnsmasq.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 inotifywait -r -m -e close_write --exclude '01-pihole.conf' --format '%w%f' /mnt/dnsmasq | while read MODFILE
 do
-    bash -c "rsync -aP --exclude '01-pihole.conf' /mnt/dnsmasq/ $CLIENTDIR --delete"
+    bash -c "rsync -aP --exclude '01-pihole.conf' /mnt/dnsmasq/ ${DNSMASQCLIENTDIR} --delete"
     if [[ "${?}" -ne "0" ]]; then
         touch /fail
     fi

84a34a69c6434a2afa177a7403376253a02422eb
goose-ws <goose@captain-kickass.net>
2021-03-10 17:52:11 -0500

    ${REM_USER} is root
    I fixed this locally but forgot to commit it before sending the push request

--- sync-dnsmasq.sh
+++ sync-dnsmasq.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 inotifywait -r -m -e close_write --exclude '01-pihole\.conf' --format '%w%f' /mnt/etc-dnsmasq.d/ | while read MODFILE
 do
-    rsync -a -P --exclude '01-pihole.conf' /mnt/etc-dnsmasq.d/ -e "ssh -p ${REM_SSH_PORT}" ${REM_USER}@${REM_HOST}:/mnt/etc-dnsmasq.d/ --delete
+    rsync -a -P --exclude '01-pihole.conf' /mnt/etc-dnsmasq.d/ -e "ssh -p ${REM_SSH_PORT}" root@${REM_HOST}:/mnt/etc-dnsmasq.d/ --delete
     if [[ "${?}" -ne "0" ]]; then
         touch /fail
     fi

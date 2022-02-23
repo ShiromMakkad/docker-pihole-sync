d2710dfe73289083cca58fe8dc0060d775bc3471
Felix Kaechele <felix@kaechele.ca>
2022-02-23 10:25:42 -0500

    Recover from unhealthy state if sync is successful
    Signed-off-by: Felix Kaechele <felix@kaechele.ca>

--- sync-pihole.sh
+++ sync-pihole.sh
@@ -4,5 +4,7 @@ do
     rsync -a -P --exclude 'setupVars.conf' --exclude 'setupVars.conf.update.bak' --exclude 'pihole-FTL.db' -e "ssh -p ${REM_SSH_PORT}" /mnt/etc-pihole/ root@${REM_HOST}:/mnt/etc-pihole/ --delete
     if [[ "${?}" -ne "0" ]]; then
         touch /fail
+    elif [[ -f "/fail" ]]; then
+	rm -f /fail
     fi
 done
